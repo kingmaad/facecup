c3d8aeb8d9a40571ca651d5ee4728d4c8aa6c475
diaoko <diaoko89@gmail.com>
2020-11-12 18:58:15 +0330

    add title to blog detail page

--- resources/views/sections/blog-post.blade.php
+++ resources/views/sections/blog-post.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <title>FaceCup.ir</title>
+    <title>FaceCup.ir - {{ $post->title }}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="keywords" />
     <meta content="" name="description" />
