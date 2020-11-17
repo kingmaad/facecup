3b76fe52ae53b34992f7a7e181a8f765a503e2c6
diaoko <diaoko89@gmail.com>
2020-11-18 02:00:14 +0330

    fix responsiv of faq page

--- resources/views/sections/faq.blade.php
+++ resources/views/sections/faq.blade.php
@@ -240,51 +240,49 @@ if (!c3.error) {
   <body>
     @include('sections.header-menu')
     <main id="main" class="main-page">
-      @if($faqs->count())
-          <section id="faq" class="wow fadeInUp p-5 m-5" style="visibility: visible; animation-name: fadeInUp;">
-            <div class="container pt-5 mt-5">
-              <div class="section-header">
-                <h2>سوالات متداول</h2>
-              </div>
-              <div class="row justify-content-center">
-                <div class="col-lg-9">
-                  <ul id="faq-list">
-                    @foreach ($faqs as $key=>$value)
-                    <li>
-                      <a data-toggle="collapse" class="collapsed" href="#faq{{ $key+1 }}">{{ $value->question }}
-                        <i class="fa fa-minus-circle"></i></a>
-                      <div id="faq{{ $key+1 }}" class="collapse" data-parent="#faq-list">
-                        <p class="pr-5 rtl">
-                          {{ $value->answer }}
-                        </p>
-                      </div>
-                    </li>
-                    @endforeach
-                  </ul>
+
+    @if($faqs->count())
+    <section id="faq" class="wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+      <div class="container">
+        <div class="section-header">
+          <h2>سوالات متداول</h2>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-9">
+            <ul id="faq-list">
+              @foreach ($faqs as $key=>$value)
+              <li>
+                <a data-toggle="collapse" class="collapsed" href="#faq{{ $key+1 }}">{{ $value->question }}
+                  <i class="fa fa-minus-circle"></i></a>
+                <div id="faq{{ $key+1 }}" class="collapse" data-parent="#faq-list">
+                  <p class="pr-5 rtl">
+                    {{ $value->answer }}
+                  </p>
                 </div>
-              </div>
-            </div>
-          </section> 
-          @endif
-     
+              </li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section> 
+    @endif
     </main>
 
-     <!--==========================
+    <!--==========================
     Footer
   ============================-->
-  <br><br><br>
-  <footer id="footer">
+    <footer id="footer">
      
 
-    <div class="container">
-      <div class="copyright">
-        © ۱۳۹9 | تمامی حقوق این وب سایت متعلق به فیسکاپ می باشد. | Powered by facecup
+      <div class="container">
+        <div class="copyright">
+          © ۱۳۹9 | تمامی حقوق این وب سایت متعلق به فیس‌کاپ می باشد. | Powered by facecup
 
+        </div>
+      
       </div>
-    
-    </div>
-  </footer>
-  <!-- #footer -->
+    </footer>
 
   <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
 
